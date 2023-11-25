17ad8c90b117b24f74f4fd56c34272188b85a6c4
Maxi <soymaxi@users.noreply.github.com>
2023-11-25 13:38:47 -0300

    updated

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'adamant'
 game 'gta5'
-description 'gta.arg core || Notifications'
+description 'gta.arg core || Notifications This is a redesign and inspiration [mythic_notify](https://github.com/JayMontana36/mythic_notify)'
 author 'Maxii - (maxi.06) on Discord'
 version '1.0'
 lua54 'yes'
